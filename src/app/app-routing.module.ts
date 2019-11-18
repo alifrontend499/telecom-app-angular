e35118d2fd6925b96333d0d47ba7d1fc647811f2
alifrontend499 <alifrontend96@gmail.com>
2019-11-18 21:45:40 +0530

    new changes 2 from home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'login', loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule)},
+  { path: 'login', loadChildren: () => import('./auth/login/login.module').then( m => m.LoginPageModule)},
 ];
 
 @NgModule({
