105e1bc04cd99ac46d89fa812eb4c4ac2439fee6
alifrontend499 <aliofficial499@gmail.com>
2019-11-19 18:18:29 +0530

    added login page to angular app

--- src/app/auth/login/login-routing.module.ts
+++ src/app/auth/login/login-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginPage } from './login.page';
 
 const routes: Routes = [
   {
-    path: 'lofin',
+    path: '',
     component: LoginPage
   }
 ];
--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  // styleUrls: ['./login.page.scss'],
+  styleUrls: ['../scss/auth-styles.scss'],
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(public loadingController: LoadingController) { }
 
   ngOnInit() {
+  }
+
+  async presentLoadingWithOptions(msg: string = "", duration: number = 2000, spinner: any = "circles") {
+    const loading = await this.loadingController.create({
+      spinner: spinner,
+      duration: duration,
+      message: msg,
+      translucent: true,
+      cssClass: 'app-loading'
+    });
+    return await loading.present();
+    // const { role, data } = await loading.onDidDismiss();
+    // console.log(data)
+  }
+
+  // handle login for submit
+  handleSubmit(ev: KeyboardEvent): void {
+    ev.preventDefault()
+    const user = 'admin'
+    const pass = 'admin'
+
+    const username = ev.target['username'].value.toLowerCase()
+    const password = ev.target['password'].value
+
+    this.presentLoadingWithOptions()
+
+    if (username !== '' && username === user && password !== '' && password === pass) {
+      console.log("object")
+    } else {
+      console.log("Username and password is required")
+    }
+
+  }
+
+  // handle login button click  
+  handleLoginClick(ev: any): void {
+    // ev.preventDefault()
   }
 
 }
